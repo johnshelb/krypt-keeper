fb37032396e0178ecb3a4bd1eb03a21e2dd038cd
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-28 13:53:28 -0400

    added single attraction home route

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -7,7 +7,19 @@ router.get("/", async (req,res) => {
     res.send("Test")
 })
 
-
+router.get("/singleAttraction/:id", async (req, res) => {
+    try {
+        res.render("singleAttraction", {
+            title: "SpookyZone", 
+            description: "It's Spooky!",
+            price: "$3,00000",
+            address: "nowhere",
+            date: "June 30"
+        })
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 // path to models folder   
 
