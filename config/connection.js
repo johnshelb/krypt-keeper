59e4e762714811332ff007fe07db6f45681d7175
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-28 13:53:15 -0400

    fixed merge conflicts

--- config/connection.js
+++ config/connection.js
@@ -6,41 +6,18 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL)
 
-<<<<<<< HEAD
 } else {
 
    sequelize = new Sequelize(
-=======
-
-
-
-
-
-} else {
-
- sequelize = new Sequelize(
-
->>>>>>> 5f7f2829e2656f36252c39b50609c6a86eb00029
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host: "localhost",
         dialect: "mysql",
-        port: 8888
+        port: 3306
 
     }
-<<<<<<< HEAD
 )};
 
 module.exports=sequelize;
-=======
-
-)
-
-
-};
-
-module.exports=sequelize;
-
->>>>>>> 5f7f2829e2656f36252c39b50609c6a86eb00029
