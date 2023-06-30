bc1ee0e8b7e75219599eddd8a07945896a8ae8ad
Blickenderfer <ecblickenderfer@gmail.com>
2023-06-29 21:59:48 -0400

    login is workinggit add -A

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const app = express();
 const exphb = require("express-handlebars");
 const hbs = exphb.create();
 const path = require("path");
+const sequelize = require("./config/connection")
+const session = require("express-session")
+const SequelizeStore = require("connect-session-sequelize")(session.Store)
 
 const PORT = process.env.PORT || 8888;
 
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
 
+app.use(session(sess))
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -19,5 +37,6 @@ app.use(express.static(path.join(__dirname,"public")));
 
 app.use(routes);
 
-
-app.listen(PORT, () => console.log("Server is Running! " + PORT));
+sequelize.sync({force:false}).then(() => {
+    app.listen(PORT, () => console.log("Server is Running! " + PORT));
+})
